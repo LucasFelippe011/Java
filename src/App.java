e0252a3f6e19535985d0bba4e380fa9b102270b0
Lucas <lucasfelippe10si@gmail.com>
2024-03-18 19:19:39 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String name = input.nextLine();
+
+        
+
+    }
+}
