39d353bafbe49f2dec061fbe6dd294c3839e933d
Lucas <lucasfelippe10si@gmail.com>
2024-03-18 21:04:04 -0300

    aula1

---
+++ src/Aula1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Aula1 {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String name = input.nextLine();
+
+        System.out.println("Muito bem "+ name);
+
+        
+
+    }
+}
